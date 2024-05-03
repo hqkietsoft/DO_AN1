3243114a70c50f1d996ffed1548af21bd6a0b2c3
DAFF <hagiakiet.work@gmail.com>
2024-05-03 07:36:02 +0700

    test

--- QuanLyNhapHang.Designer.cs
+++ QuanLyNhapHang.Designer.cs
@@ -28,31 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(118, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(285, 21);
-            this.textBox1.TabIndex = 0;
             // 
             // QuanLyNhapHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 404);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1031, 571);
             this.Name = "QuanLyNhapHang";
             this.Text = "QuanLyNhapHang";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
