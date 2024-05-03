02e2e755c466e40bbff8fed08430f831e265184b
lehuyen <huyenltt.a5k46gtb@gmail.com>
2024-05-03 22:57:20 +0700

    Upate Form Nguoi Dung

--- QuanLyNguoiDung.Designer.cs
+++ QuanLyNguoiDung.Designer.cs
@@ -28,66 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.Taikhoan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Matkhau = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ngáyinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChucVu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgv1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4});
-            this.dataGridView1.Location = new System.Drawing.Point(152, 85);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(442, 202);
-            this.dataGridView1.TabIndex = 0;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Taikhoan,
+            this.Matkhau,
+            this.MaNV,
+            this.HoTen,
+            this.Ngáyinh,
+            this.GioiTinh,
+            this.ChucVu,
+            this.DiaChi});
+            this.dgv1.Location = new System.Drawing.Point(125, 60);
+            this.dgv1.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.RowHeadersWidth = 51;
+            this.dgv1.Size = new System.Drawing.Size(549, 322);
+            this.dgv1.TabIndex = 0;
+            this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellContentClick);
             // 
-            // Column1
+            // Taikhoan
             // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.Name = "Column1";
+            this.Taikhoan.DataPropertyName = "Taikhoan";
+            this.Taikhoan.HeaderText = "Taikhoan";
+            this.Taikhoan.MinimumWidth = 6;
+            this.Taikhoan.Name = "Taikhoan";
+            this.Taikhoan.Width = 125;
             // 
-            // Column2
+            // Matkhau
             // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.Name = "Column2";
+            this.Matkhau.DataPropertyName = "Matkhau";
+            this.Matkhau.HeaderText = "Matkhau";
+            this.Matkhau.MinimumWidth = 6;
+            this.Matkhau.Name = "Matkhau";
+            this.Matkhau.Width = 125;
             // 
-            // Column3
+            // MaNV
             // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.Name = "Column3";
+            this.MaNV.DataPropertyName = "MaNV";
+            this.MaNV.HeaderText = "MaNV";
+            this.MaNV.MinimumWidth = 6;
+            this.MaNV.Name = "MaNV";
+            this.MaNV.Width = 125;
             // 
-            // Column4
+            // HoTen
             // 
-            this.Column4.HeaderText = "Column4";
-            this.Column4.Name = "Column4";
+            this.HoTen.DataPropertyName = "HoTen";
+            this.HoTen.HeaderText = "HoTen";
+            this.HoTen.MinimumWidth = 6;
+            this.HoTen.Name = "HoTen";
+            this.HoTen.Width = 125;
+            // 
+            // Ngáyinh
+            // 
+            this.Ngáyinh.DataPropertyName = "NgaySinh";
+            this.Ngáyinh.HeaderText = "NgaySinh";
+            this.Ngáyinh.MinimumWidth = 6;
+            this.Ngáyinh.Name = "Ngáyinh";
+            this.Ngáyinh.Width = 125;
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.HeaderText = "GioiTinh";
+            this.GioiTinh.MinimumWidth = 6;
+            this.GioiTinh.Name = "GioiTinh";
+            this.GioiTinh.Width = 125;
+            // 
+            // ChucVu
+            // 
+            this.ChucVu.DataPropertyName = "ChucVu";
+            this.ChucVu.HeaderText = "ChucVu";
+            this.ChucVu.MinimumWidth = 6;
+            this.ChucVu.Name = "ChucVu";
+            this.ChucVu.Width = 125;
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.HeaderText = "DiaChi";
+            this.DiaChi.MinimumWidth = 6;
+            this.DiaChi.Name = "DiaChi";
+            this.DiaChi.Width = 125;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(27, 432);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "&Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(264, 431);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Location = new System.Drawing.Point(125, 30);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(100, 23);
+            this.txtMaNV.TabIndex = 4;
+            this.txtMaNV.TextChanged += new System.EventHandler(this.txtXoa_TextChanged);
             // 
             // QuanLyNguoiDung
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 411);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(817, 506);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.dgv1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "QuanLyNguoiDung";
             this.Text = "QuanLyNguoiDung";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.QuanLyNguoiDung_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Taikhoan;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Matkhau;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ngáyinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ChucVu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtMaNV;
     }
 }
--- QuanLyNguoiDung.cs
+++ QuanLyNguoiDung.cs
@@ -3,19 +3,78 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace DO_AN_1
 {
     public partial class QuanLyNguoiDung : DevExpress.XtraEditors.XtraForm
     {
+        SqlConnection con;
+        SqlCommand cmd;
+        public SqlConnection Moketnoi()
+        {
+            string str = "Data Source=DESKTOP-H2UCOT4\\SQLEXPRESS;Initial Catalog=QuanLyKinhDoanhMayTinh;Integrated Security=True";
+            con = new SqlConnection(str);
+            con.Open();
+            return con;
+        }
+        public void Dongketnoi()
+        {
+            con.Close();
+        }
+
         public QuanLyNguoiDung()
         {
             InitializeComponent();
+            Hienthi_dgv1();
+        }
+        public DataTable ShowAll()
+        {
+            string getAllQuery = "select * from NguoiDung";
+            cmd = new SqlCommand(getAllQuery,Moketnoi());
+            SqlDataReader dr = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(dr);
+            return table ;
+        }
+        public void do_sql(string Query)
+        {
+            cmd = new SqlCommand(Query,Moketnoi());
+            cmd.ExecuteNonQuery();
+            Dongketnoi();
+        }
+        public void Hienthi_dgv1()
+        {
+            dgv1.DataSource = ShowAll();
+        }
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaNV.Text = dgv1.CurrentRow.Cells[2].Value.ToString();
+
+        }
+
+        private void QuanLyNguoiDung_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string Query = "DELETE FROM NguoiDung WHERE MaNV = '" + txtMaNV.Text + "' ";
+            do_sql(Query);
+            Hienthi_dgv1();
+        }
+
+        private void txtXoa_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
